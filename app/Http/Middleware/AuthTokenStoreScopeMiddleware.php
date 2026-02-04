5de1d03412639a296f7afe257d6cf976959cb58c
Aws12505 <awsaljaramani12505@gmail.com>
2026-02-04 12:21:56 -0500

    ..

--- app/Http/Middleware/AuthTokenStoreScopeMiddleware.php
+++ app/Http/Middleware/AuthTokenStoreScopeMiddleware.php
@@ -44,7 +44,20 @@ class AuthTokenStoreScopeMiddleware
         $cache = Cache::store('redis');
         $cacheKey = $this->verifyCacheKey($serviceName, $userToken, $request, $storeContext);
 
-        $verify = $cache->remember($cacheKey, $cacheTtl, function () use (
+        // $verify = $cache->remember($cacheKey, $cacheTtl, function () use (
+        //     $baseUrl,
+        //     $verifyPath,
+        //     $serviceName,
+        //     $callToken,
+        //     $timeout,
+        //     $retries,
+        //     $retryMs,
+        //     $userToken,
+        //     $request,
+        //     $storeContext
+        // ) {
+        //     return
+        $verify =   $this->verifyWithAuthServer(
             $baseUrl,
             $verifyPath,
             $serviceName,
@@ -55,20 +68,8 @@ class AuthTokenStoreScopeMiddleware
             $userToken,
             $request,
             $storeContext
-        ) {
-            return $this->verifyWithAuthServer(
-                $baseUrl,
-                $verifyPath,
-                $serviceName,
-                $callToken,
-                $timeout,
-                $retries,
-                $retryMs,
-                $userToken,
-                $request,
-                $storeContext
-            );
-        });
+        );
+        // });
 
         // 5) Enforce BOTH token validity + authorization decision
         $active = (bool) ($verify['active'] ?? false);
